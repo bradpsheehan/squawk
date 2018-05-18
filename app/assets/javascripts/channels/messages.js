364a79003b7337a33660ead7ed233133e31deed7
Bradley Sheehan <bsheehan@ampion.net>
2018-05-18 15:23:59 -0400

    Submit form with keypress and clear input

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,6 +1,13 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
     received: function(data) {
         $("#messages").removeClass('hidden')
+        var $message_form = $("#new_message")
+        $message_form[0].reset()
+        $("#message_submit").onkeydown = function(e){
+          if(e.keyCode == 13){
+            $message_form.submit()
+          }
+        };
         return $('#messages').append(this.renderMessage(data));
     },
 
